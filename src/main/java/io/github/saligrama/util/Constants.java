f78bba28136e61ad4c0387e2069d7b8783659977
Aditya Saligrama <saligrama427@gmail.com>
2018-03-31 02:43:07 -0400

    Minor fixes to license in source files

--- src/main/java/io/github/saligrama/util/Constants.java
+++ src/main/java/io/github/saligrama/util/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 Aditya Saligrama.
+ * Copyright (c) 2017-2018 Aditya Saligrama.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
